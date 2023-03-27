12823ae831020bfa24e436cdc742cac0b3424565
doug-coder64 <douglaspeterson6@gmail.com>
2023-03-27 11:28:37 -0500

    Added tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,33 @@ import { gql, useQuery } from "@apollo/client";
 import type { Link } from "@prisma/client";
 
 const AllLinksQuery = gql`
-  query {
-    links {
-      id
-      Url
-      altText
+  query AllLinksQuery($first: int, $after: ID) {
+    links(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        cursor
+      }
+      node {
+        id
+        Url
+        altText
+      }
     }
   }
 `;
 
 const Home: NextPage = () => {
-  const { data, loading, error } = useQuery(AllLinksQuery);
+  const { data, loading, error, fetchMore } = useQuery(AllLinksQuery, {
+    variables: { first: 2 },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
+
+  const { endCursor, hasNextPage } = data.links.pageInfo;
 
   return (
     <div>
@@ -30,22 +43,38 @@ const Home: NextPage = () => {
 
       <div className='container mx-auto max-w-5xl my-20'>
         <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-          {data.links.map((link: Link) => (
-            <li key={link.id} className='shadow  max-w-md  rounded'>
-              <img
-                className='shadow-sm'
-                src={
-                  link.Url === null
-                    ? "../../src/images/imageNotFound.png"
-                    : link.Url
-                }
-              />
+          {data?.links.edges.map(({ node }: { node: Link }) => (
+            <li key={node.id} className='shadow  max-w-md  rounded'>
+              <img className='shadow-sm' src={node.Url ? node.Url : ""} />
               <div className='p-5 flex flex-col space-y-2'>
-                <p className='text-gray-600'>{link.altText}</p>
+                <p className='text-gray-600'>{node.altText}</p>
               </div>
             </li>
           ))}
         </ul>
+        {hasNextPage ? (
+          <button
+            className='px-4 py-2 bg-blue-500 text-white rounded my-10'
+            onClick={() => {
+              fetchMore({
+                variables: { after: endCursor },
+                updateQuery: (prevResult, { fetchMoreResult }) => {
+                  fetchMoreResult.links.edges = [
+                    ...prevResult.links.edges,
+                    ...fetchMoreResult.links.edges,
+                  ];
+                  return fetchMoreResult;
+                },
+              });
+            }}
+          >
+            more
+          </button>
+        ) : (
+          <p className='my-10 text-center font-medium'>
+            You've reached the end!{" "}
+          </p>
+        )}
       </div>
     </div>
   );
