81f9b898d2a846c2e5a49d06e449d2721b95df48
doug-coder64 <douglaspeterson6@gmail.com>
2023-05-04 21:42:53 -0500

    fix Head issue

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Header from "./Header";
-import Head from "next/head";
 interface Props {
   children: React.ReactNode;
 }
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className='root'>
-      <Head>
-        <title>React Social Media</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
       <div className='hidden'></div>
       <Header />
       {children}
